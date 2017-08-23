b180481dad6eb6cb984cb796d6ae02e29feda222
Qiang Zhou <chinaboy@udel.edu>
2017-08-23 19:50:24 -0400

    snapshot

---
+++ src/main/resources/static/js/hello.js
@@ -0,0 +1,53 @@
+angular.module('hello', ['ngRoute'])
+  .config(function($routeProvider, $httpProvider){
+
+    $routeProvider.when('/', {
+        templateUrl: 'home.html',
+        controller: 'home',
+        controllerAs: 'controller'
+    }).when('login', {
+        templateUrl: 'login.html',
+        controller: 'navigation',
+        controllerAs: 'controller'
+    }).otherwise('/');
+
+    $httpProvider.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+  })
+  .controller('home', ['$scope', '$http', function($scope, $http) {
+    var self = this;
+    $http.get('/resource/').then(function(response){
+        self.greeting = response.data;
+    });
+  }])
+  .controller('navigation', ['$scope', '$http', function($scope, $http){
+    var self = this;
+    var authenticate = function(credentials, callback){
+        var headers = credentials ? {authorization : "Basic " + btoa(credentials.username + ":" + credentials.password)} : {};
+
+        $http.get('user', {headers : headers}).then(function(response){
+            if(response.data.name){
+                $rootScope.authenticated = true;
+            }else{
+                $rootScope.authenticated = false;
+            }
+            callback && callback();
+        }, function(){
+            $rootScope.authenticated = false;
+            callback && callback();
+        });
+    }
+
+    authenticate();
+    self.credentials = {};
+    self.login = function(){
+        authenticate(self.credentials, function(){
+            if($rootScope.authenticated){
+                $location.path("/");
+                self.error = false;
+            } else {
+                $location.path("/login");
+                self.error = true;
+            }
+        })
+    }
+  }]);
